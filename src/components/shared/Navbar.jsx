ca03f9ed746fa1dcd247804959c80ba624a8e779
smirk1795 <hardikpatel.hardik36@gmail.com>
2018-05-13 15:13:59 -0400

    Fix Navbar. Fonts to Roboto Mono.

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -17,20 +17,15 @@ class Navbar extends React.Component {
 
                     <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                         <ul className="nav navbar-nav">
+                            <li><a href="/faculties">Faculties</a></li>
                             <li><a href="/students">Students</a></li>
-                            <li><a href="#">Link</a></li>
-                            <li className="dropdown">
-                                <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span className="caret"></span></a>
-                                <ul className="dropdown-menu">
-                                    <li><a href="#">Action</a></li>
-                                    <li><a href="#">Another action</a></li>
-                                    <li><a href="#">Something else here</a></li>
-                                    <li role="separator" className="divider"></li>
-                                    <li><a href="#">Separated link</a></li>
-                                    <li role="separator" className="divider"></li>
-                                    <li><a href="#">One more separated link</a></li>
-                                </ul>
-                            </li>
+                            <li><a href="/attendance">Attendance</a></li>
+                            <li><a href="/batches">Batches</a></li>
+                            <li><a href="sales">Sales</a></li>
+                            <li><a href="levels">Levels</a></li>
+                            <li><a href="ranks">Ranks</a></li>
+                            <li><a href="progresses">Progresses</a></li>
+
                         </ul>
                         <ul className="nav navbar-nav navbar-right">
                             <li><a href="/login">Login</a></li>                                
