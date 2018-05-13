d0d37c7683793e7e0ca7bb15b6417febd972dd7d
smirk1795 <hardikpatel.hardik36@gmail.com>
2018-05-13 17:47:44 -0400

    Add Authentication.

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,41 @@
 import React from 'react'
+import auth from '../services/auth'
+import * as authenticate from 'endpoints/authentication'
+import alertify from 'alertifyjs';
 import Navbar from 'src/components/shared/Navbar'
 
 class Login extends React.Component {
+    constructor(props) {
+        super(props)
+        this.update = this.update.bind(this)
+        this.submit = this.submit.bind(this)
+        this.state = {
+            email: '',
+            password: ''
+        }
+    }
+
+    update(el) {
+        this.setState({
+          [el.target.id]: el.target.value
+        })
+    }
+
+    submit(e) {
+        e.preventDefault();
+        if (this.state.email && this.state.password) {
+            authenticate.authenticate(this.state).then(res => {
+                if (res) {
+                    auth.login(res.data.data.accessToken)
+                }
+            }).catch(err => {
+                if (err) {
+                    alertify.error('Check your email or password!')
+                }
+            })
+        }
+    }
+
     render() {
         return (
             <div>
@@ -11,23 +45,25 @@ class Login extends React.Component {
                         <div className="col-md-4 col-md-offset-4">
                             <form className="form-signin" autoComplete="off">
                                 <h2 className="form-signin-heading">Please sign in</h2>
-                                <label htmlFor="inputEmail" className="sr-only">Email address</label>
+                                <label htmlFor="email" className="sr-only">Email address</label>
                                 <input
                                     type="email"
-                                    id="inputEmail"
+                                    id="email"
                                     className="form-control"
                                     placeholder="Email address"
                                     required=""
-                                    autoFocus="" />
-                                <label htmlFor="inputPassword" className="sr-only">Password</label>
-                                <input type="password" id="inputPassword" className="form-control margin-top-20" placeholder="Password"
-                                    required=""/>
+                                    autoFocus=""
+                                    onChange={this.update}
+                                />
+                                <label htmlFor="email" className="sr-only">Password</label>
+                                <input type="password" id="password" className="form-control margin-top-20" placeholder="Password"
+                                    required="" onChange={this.update}/>
                                 <div className="checkbox">
                                     <label>
                                         <input type="checkbox" value="remember-me" /> Remember me
                                 </label>
                                 </div>
-                                <button className="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+                                <button className="btn btn-lg btn-primary btn-block" onClick={this.submit}>Sign in</button>
                             </form>
 
                         </div>
--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,6 @@ import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 import AuthContainer from 'src/components/Auth/AuthContainer'
 import auth from './services/auth'
-import HomePage from 'src/components/HomePage'
 import Login from 'src/components/Login'
 import Students from 'src/components/Students'
 
@@ -10,8 +9,7 @@ export default (
   <div>
     <Switch>
       <Route exact path="/login" component={Login} />
-      <AuthContainer canAccess={auth.authenticated} exact path='/' component={HomePage} />
-      <AuthContainer canAccess={auth.authenticated} exact path='/students' component={Students} />
+      <AuthContainer canAccess={auth.authenticated} exact path='/' component={Students} />
     </Switch>
   </div>
 )
