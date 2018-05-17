f470465f990d340c9b52c5b9fc2266b0f197d8be
parth7676 <parth7676@gmail.com>
2018-05-17 18:27:00 -0400

    Solved problem with navbar selected item and gave minimum hight to container.

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -3,12 +3,17 @@ import auth from '../../services/auth'
 import { withRouter } from 'react-router-dom'
 
 class Navbar extends React.Component {
-    constructor (props) {
+    constructor(props) {
         super(props)
     }
 
     componentDidMount() {
-        const item = this.props.match.url
+        var item = this.props.match.url
+        if (item == '/') {
+            item = 'students'
+        } else {
+            item = item.split('/')[1]
+        }
         this.refs[item].classList.add('active')
     }
 
@@ -32,15 +37,15 @@ class Navbar extends React.Component {
 
                     <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                         <ul className="nav navbar-nav">
-                            <li ref={"/about"}><a href="/about">About</a></li>
-                            {auth.authenticated && <li ref={"/faculties"}><a href="/faculties">Faculties</a></li>}
-                            {auth.authenticated && <li ref={"/"}><a href="/">Students</a></li>}
-                            {auth.authenticated && <li ref={"/attendance"}><a href="/attendance">Attendance</a></li>}
-                            {auth.authenticated && <li ref={"/batches"}><a href="/batches">Batches</a></li>}
-                            {auth.authenticated && <li ref={"/sales"}><a href="/sales">Sales</a></li>}
-                            {auth.authenticated && <li ref={"/levels"}><a href="/levels">Levels</a></li>}
-                            {auth.authenticated && <li ref={"/ranks"}><a href="/ranks">Ranks</a></li>}
-                            {auth.authenticated && <li ref={"/progresses"}><a href="/progresses">Progresses</a></li>}
+                            <li ref={"about"}><a href="/about">About</a></li>
+                            {auth.authenticated && <li ref={"faculties"}><a href="/faculties">Faculties</a></li>}
+                            {auth.authenticated && <li ref={"students"}><a href="/">Students</a></li>}
+                            {auth.authenticated && <li ref={"attendance"}><a href="/attendance">Attendance</a></li>}
+                            {auth.authenticated && <li ref={"batches"}><a href="/batches">Batches</a></li>}
+                            {auth.authenticated && <li ref={"sales"}><a href="/sales">Sales</a></li>}
+                            {auth.authenticated && <li ref={"levels"}><a href="/levels">Levels</a></li>}
+                            {auth.authenticated && <li ref={"ranks"}><a href="/ranks">Ranks</a></li>}
+                            {auth.authenticated && <li ref={"progresses"}><a href="/progresses">Progresses</a></li>}
                         </ul>
                         <ul className="nav navbar-nav navbar-right" style={{ cursor: 'pointer' }}>
                             {
