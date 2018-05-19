71fbaaad855db669a38970ffb32815a35131c833
smirk1795 <hardikpatel.hardik36@gmail.com>
2018-05-18 20:54:59 -0400

    fix select css.

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'
 import {BrowserRouter} from 'react-router-dom'
 import routes from './routes'
 import 'css/main.scss'
+import 'react-select/dist/react-select.css';
 import "react-bootstrap-table/css/react-bootstrap-table.css"
 import 'bootstrap-sass/assets/javascripts/bootstrap.min.js'
 
