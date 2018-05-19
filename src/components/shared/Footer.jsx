18dc38dc06bddf47158dd4481e32c049c8b3dd2c
smirk1795 <hardikpatel.hardik36@gmail.com>
2018-05-19 13:12:45 -0400

    fix footer.

--- src/components/shared/Footer.jsx
+++ src/components/shared/Footer.jsx
@@ -3,20 +3,21 @@ import React from 'react'
 class Footer extends React.Component {
     render() {
         return (
-            <footer className="navbar-fixed-bottom footer">
+            <div className="navbar-inverse footer">
                 <div className="container">
-                    <div className="row">
-                        <div className="col-md-4">
-                            <p className="text-muted"><strong>KungFu Yoga</strong></p>
+                    <footer>
+                        <div className="row margin-top-20">
+                            <div className="col-md-4">
+                                <p className="text-muted"><strong>KungFu Yoga</strong></p>
+                            </div>
+                            <div className="col-md-4"/>
+                            <div className="col-md-4">
+                                <p>Made with <span className="glyphicon glyphicon-heart" style={{ color: 'red' }}></span> by Hardik, Parth and Siddharth</p>
+                            </div>
                         </div>
-                        <div className="col-md-4"></div>
-                        <div className="col-md-4">
-                            <p>Made with <span className="glyphicon glyphicon-heart" style={{ color: 'red' }}></span> by Hardik, Parth and Siddharth</p>
-                        </div>
-                    </div>
-
+                    </footer>
                 </div>
-            </footer>
+            </div>
         )
     }
 }
